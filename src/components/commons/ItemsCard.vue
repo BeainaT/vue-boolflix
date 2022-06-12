86b0de7f49893e36128c08fb5c3d7b273938b7db
BeainaT <tania.df@hotmail.it>
2022-06-12 15:32:01 +0200

    mls 3 completed+refact ItemCard logic+basis mls4

--- src/components/commons/ItemsCard.vue
+++ src/components/commons/ItemsCard.vue
@@ -2,13 +2,16 @@
   <ul class="item p-0 m-0 list-unstyled">
     <li class="img_box">
         <img class="poster_item" :src="getPoster(film.poster_path)" alt="film poster">
-        <div class="descriptions_item">
-            <p>{{film.title}}</p>
-            <p>{{film.name}}</p>
-            <p>{{film.original_title}}</p>
-            <p>{{film.original_name}}</p>
-            <p>{{film.vote_average}}</p>
-            <lang-flag :iso="film.original_language" :alt="film.original_language"/>
+        <div class="descriptions_item py-4 px-2 py-3 overflow-auto">
+            <p><strong>Titolo:</strong>{{film.title || film.name}}</p>
+            <p><strong>Titolo originale:</strong>{{film.original_title || film.original_name}}</p>
+            <div class="vote">
+                <strong>Voto:</strong>
+                <i class="fa-solid fa-star" v-for="(star, i) in getStars(film.vote_average)" :key="'star' + i"></i>
+                <i class="fa-regular fa-star" v-for="(emptyStar, i) in 5 - getStars(film.vote_average)" :key="'emptyStar' + i"></i>
+            </div>
+            <lang-flag :iso="film.original_language" :alt="film.original_language" class="d-block"/>
+            <p><strong>Trama:</strong>{{getOverview(film.overview)}}</p>
         </div>
     </li>
   </ul>
@@ -34,8 +37,14 @@ export default {
     methods: {
         getPoster(path) {
             return path == null ? 'https://moviereelist.com/wp-content/uploads/2019/07/poster-placeholder.jpg' : 'https://image.tmdb.org/t/p/w342'+path;
+        },
+        getStars(elm) {
+            return Math.ceil((elm / 2));
+        },
+        getOverview(elm) {
+            return elm === "" ? `Sorry, no plot available for this item` : elm;
         }
-    }
+    },
 }
 </script>
 
@@ -63,6 +72,9 @@ export default {
                     height: auto;
                     background-image: url(https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/World_Flag_%282004%29.svg/2560px-World_Flag_%282004%29.svg.png);
                     background-size: cover;
+                }
+                > * {
+                    margin: 0;
                 }
             }
 
